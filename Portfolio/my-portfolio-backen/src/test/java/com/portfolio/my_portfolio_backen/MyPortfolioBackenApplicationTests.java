9184438506f53cf382126b8cacbb018ccf817c91
IsraelHernandezFES <jesushernandezpacheco0705@gmail.com>
2026-06-25 21:10:56 -0600

    se agrega el proyecto 1 Portfolio

---
+++ Portfolio/my-portfolio-backen/src/test/java/com/portfolio/my_portfolio_backen/MyPortfolioBackenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.my_portfolio_backen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioBackenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
